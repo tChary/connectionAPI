b3b25a58786f0ac108b5a5944ac3d77e8b1ec834
Lance Hunter <robert.l.hunter@oracle.com>
2019-02-21 09:17:04 -0500

    Adding franchise.js

---
+++ routes/franchise.js
@@ -0,0 +1,40 @@
+const express = require(`express`);
+const router = express.Router();
+
+const dbConfig = require(`./dbconfig`);
+const oracledb = require(`oracledb`);
+
+
+/* GET users listing. */
+router.get(`/`, function (req, res) {
+  oracledb.getConnection({
+    user: dbConfig.dbuser,
+    password: dbConfig.dbpassword,
+    connectString: dbConfig.connectString
+  }, (err, connection) => {
+    if (err) {
+      res.send(err);
+      doRelease(connection);
+      return;
+    }
+    connection.execute(`SELECT * FROM franchise`, [], (err, result) => {
+      if (err) {
+        res.send(err);
+        doRelease(connection);
+        return;
+      }
+      res.send(result);
+      doRelease(connection);
+    });
+  });
+});
+
+function doRelease(connection) {
+  connection.close(
+    function (err) {
+      if (err)
+        console.error(err.message);
+    });
+}
+
+module.exports = router;
